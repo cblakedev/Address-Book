6c073114b5edff03510481e435bc887eef8c6c39
Chris Blake <cblake2335@gmail.com>
2024-08-27 01:49:28 -0400

    Style and initial mobile responsiveness update.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-	<div class="container pb-2 max-w-screen-md mx-auto font-sans h-full">
-		<div class="h-[10%] flex flex-col justify-center items-center">
-			<h1 class="text-4xl font-bold text-[#4158A6]">Address Book</h1>
+	<div class="container pt-[100px] md:pb-2 max-w-screen-md mx-auto font-sans h-full">
+		<div class="flex flex-row absolute top-0 left-0 items-center w-full py-3 bg-white custom-bottom-shadow">
+			<img class="width-[25px] h-[25px] pl-3" src="/genesys-logo.png" alt="Address Book Logo">
+			<h1 class="text-3xl font-bold text-[#4158A6] px-3">Address Book</h1>
 		</div>
-		<div class="flex flex-row h-[90%] shadow-lg mx-4">
+		<div class="flex flex-row justify-center h-full shadow-lg border-grey-100 md:border md:mx-4 rounded-md">
 			<contactsListPage />
 			<contactProfilePage />
 		</div>
--- src/components/contactsList/contactsListPage.vue
+++ src/components/contactsList/contactsListPage.vue
@@ -10,34 +10,39 @@ export default {
             searchText: '',
             sortDirection: 'asc',
             people: [],
-            searchTimeout: null
+            searchTimeout: null,
+            isMobile: false
         }
     },
     computed: {
         groupedContacts() {
             let sections = {};
 
-            this.people.forEach((contact) => {
-                let firstLetter = contact.name.charAt(0).toUpperCase();
+            if (this.people && !this.people.message) {
+                this.people.forEach((contact) => {
+                    let firstLetter = contact.name.charAt(0).toUpperCase();
 
-                if (!sections[firstLetter]) {
-                    sections[firstLetter] = [];
-                }
+                    if (!sections[firstLetter]) {
+                        sections[firstLetter] = [];
+                    }
 
-                // Push an object with both id and name
-                sections[firstLetter].push({ id: contact.id, name: contact.name });
-            });
+                    // Push an object with both id and name
+                    sections[firstLetter].push({ id: contact.id, name: contact.name });
+                });
 
-            // Sort the sections alphabetically and adjust sorting order
-            const sortedSections = Object.keys(sections)
-                .sort((a, b) => this.sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a))
-                .reduce((sortedSections, key) => {
-                    // Sort each section based on name property of the objects
-                    sortedSections[key] = sections[key].sort((a, b) => this.sortDirection === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
-                    return sortedSections;
-                }, {});
+                // Sort the sections alphabetically and adjust sorting order
+                const sortedSections = Object.keys(sections)
+                    .sort((a, b) => this.sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a))
+                    .reduce((sortedSections, key) => {
+                        // Sort each section based on name property of the objects
+                        sortedSections[key] = sections[key].sort((a, b) => this.sortDirection === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
+                        return sortedSections;
+                    }, {});
 
-            return sortedSections;
+                return sortedSections;
+            }
+
+            return []
         }
     },
     watch: {
@@ -48,7 +53,7 @@ export default {
             // Set a new timeout to delay the search
             this.searchTimeout = setTimeout(() => {
                 this.getAllPeople(newSearchText);
-            }, 600); // 600ms debounce
+            }, 500); // 500ms debounce
         }
     },
     methods: {
@@ -58,6 +63,9 @@ export default {
                     this.$store.commit('updateSelectedContact', data);
                 });
         },
+        mobileGetContactProfileOnClick(id) {
+            console.log('this is a mobile click');
+        },
         getAllPeople() {
             contactListPageEndpoints.getAllPeople(`http://localhost:8080/api/people?name=${this.searchText}`)
                 .then((data) => {
@@ -66,10 +74,23 @@ export default {
         },
         toggleSortDirectionOnClick() {
             this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+        },
+        checkScreenSize() {
+            this.isMobile = window.innerWidth < 768;
         }
     },
     created() {
         this.getAllPeople();
+    },
+    mounted() {
+        // Check the initial screen size when the component is mounted
+        this.checkScreenSize();
+        // Add an event listener to update isMobile on window resize
+        window.addEventListener('resize', this.checkScreenSize);
+    },
+    beforeUnmount() {
+        // Remove the event listener to prevent memory leaks
+        window.removeEventListener('resize', this.checkScreenSize);
     }
 }
 </script>
