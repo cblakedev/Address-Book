b8e8ebafc0882f1eb1ca01bb2c7165021b56e3fd
Chris Blake <cblake2335@gmail.com>
2024-08-29 02:29:53 -0400

    Fixed bottom and top margin of the parent component.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container pt-[60px] xs:pt-[100px] md:pb-2 max-w-screen-md mx-auto font-sans h-full">
+    <div class="container pt-[60px] xs:pt-20 md:pb-5 max-w-screen-md mx-auto font-sans h-full">
         <div class="flex flex-row absolute top-0 left-0 items-center w-full py-3 bg-white custom-bottom-shadow">
             <img class="width-[25px] h-[25px] pl-3" src="/genesys-logo.png" alt="Address Book Logo">
             <h1 class="text-3xl font-bold text-[#4158A6] px-3">Address Book</h1>
